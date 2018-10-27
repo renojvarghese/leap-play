8cca240d6c9f42b32b1dc061d0c4748405d32ff8
Renoj Varghese <renoj.varghese@uconn.edu>
2018-10-27 15:11:25 -0400

    adding packags for scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,32 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin"); //installed via npm
-const webpack = require("webpack"); //to access built-in plugins
+const webpack = require("webpack");
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const babel = {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: {
+        loader: "babel-loader"
+    }
+};
+const scss = {
+    test: /\.scss$/,
+    use: [
+        MiniCssExtractPlugin.loader,
+        "css-loader",
+        {
+            loader: "clean-css-loader",
+            options: {
+                compatibility: "ie9",
+                level: 2,
+                inline: ["remote"]
+            }
+        },
+        "postcss-loader",
+        "sass-loader"
+    ]
+};
 const config = {
     entry: "./src/main.js",
     output: {
@@ -9,18 +34,14 @@ const config = {
         filename: "bundle.js"
     },
     module: {
-        rules: [
-            { test: /\.txt$/, use: "raw-loader" },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
+        rules: [babel, scss]
     },
-    plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })]
+    plugins: [
+        new HtmlWebpackPlugin({ template: "./src/index.html" }),
+        new MiniCssExtractPlugin({
+            filename: "main.css"
+        })
+    ]
 };
 
 module.exports = config;
