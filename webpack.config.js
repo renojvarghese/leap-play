c496b9d98860582f0a4fa3655b67be4fbc2f52f6
Renoj Varghese <renoj.varghese@uconn.edu>
2018-10-27 14:10:17 -0400

    finishing up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,26 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-module.exports = {
+const HtmlWebpackPlugin = require("html-webpack-plugin"); //installed via npm
+const webpack = require("webpack"); //to access built-in plugins
+const path = require("path");
+
+const config = {
+    entry: "./src/main.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
     module: {
         rules: [
+            { test: /\.txt$/, use: "raw-loader" },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
                 }
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: "html-loader"
-                    }
-                ]
             }
         ]
     },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        })
-    ]
+    plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })]
 };
+
+module.exports = config;
