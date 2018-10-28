e28e4d31cc598915f99c165a44b5e6eb861509c3
Renoj Varghese <renoj.varghese@uconn.edu>
2018-10-28 13:39:42 -0400

    adding hands objects

---
+++ src/hands.js
@@ -0,0 +1,31 @@
+let hue = 30;
+let radius = 20;
+var currEl = null;
+var onPress = false;
+var willClick = true;
+var active = false;
+
+export default [
+    {
+        hue: 30,
+        defaultHue: 30,
+        pressHue: 80,
+        cursor: "hand1",
+        radius: radius,
+        currEl: null,
+        onPress: false,
+        willCLick: true,
+        active: false
+    },
+    {
+        hue: 120,
+        defaultHue: 120,
+        pressHue: 170,
+        cursor: "hand2",
+        radius: radius,
+        currEl: null,
+        onPress: false,
+        willCLick: true,
+        active: false
+    }
+];
